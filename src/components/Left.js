88b55d2454883a34a719624b6be1ac808fd34bd8
Mark Kennedy <kennedymark@hotmail.com>
2022-06-04 09:38:07 -0400

    started top text

--- src/components/Left.js
+++ src/components/Left.js
@@ -5,6 +5,14 @@ const Left = () => {
   return (
     <div className="left">
       <img src={Picture} alt="Mark" className="profilePicture" />
+      <h3>ABOUT ME</h3>
+      <p>
+        I am an accomplished helicopter pilot seeking new challenges. Over the
+        past two years my curiosity has turned my hobby into a career path. I’ve
+        decided to pursue a field which plays to my strengths of being a
+        creative, driven problem solver. What I pride myself most is my ability
+        learn, and to know more today than I did yesterday.
+      </p>
     </div>
   )
 }
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export const Top = () => {
-  return <div className="top">Top</div>
+  return (
+    <div className="top">
+      <div className="top-left">
+        <h1>Mark</h1>
+        <h1>Kennedy</h1>
+        <h3>Software Engineer</h3>
+        <h3>Designer</h3>
+        <h3>Helicopter Pilot</h3>
+      </div>
+      <div className="top-right">
+        <h3>kennedymark680@gmail.com</h3>
+        <h3>linkedin.com/in/kennedymark680</h3>
+        <h3>kennedymark680</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Top
