51b929e4e14530523525793c99fa900b71f6d591
Mark Kennedy <kennedymark@hotmail.com>
2022-06-05 13:33:09 -0400

    creating project switch

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import { projects } from '../resources/ProjectData'
+import { useState } from 'react'
 import Project from './Project'
 
 const ProjectsDisplay = () => {
+  const [selectedProject, setSelectedProject] = useState('')
+
+  let display
+
+  switch (selectedProject) {
+    case 'PennStar4':
+      display = <Project project={projects[2]} />
+      break
+    case 'Space Pilgrims':
+      display = <Project project={projects[1]} />
+      break
+    default:
+      display = <Project project={projects[0]} />
+      break
+  }
+
   return (
     <div className="projectsDisplay">
       <div className="projectButtons">
         {projects.map((project) => (
-          <button>{project.title}</button>
+          <button onClick={() => setSelectedProject(project.title)}>
+            {project.title}
+          </button>
         ))}
       </div>
-      <div className="projectInfo">M</div>
+      <div className="projectInfo">m</div>
     </div>
   )
 }
